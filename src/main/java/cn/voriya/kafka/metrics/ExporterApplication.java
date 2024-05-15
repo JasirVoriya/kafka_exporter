95f6eb9534ac7ed8bedf2f29329767a9576abac3
JasirVoriya <jasirvoriya@gmail.com>
2024-05-15 10:33:26 +0800

    fix:httpServer使用try-resources管理

--- src/main/java/cn/voriya/kafka/metrics/ExporterApplication.java
+++ src/main/java/cn/voriya/kafka/metrics/ExporterApplication.java
@@ -18,8 +18,15 @@ public class ExporterApplication {
         }
         log.info("broker list: {}", args[0]);
         Config.BROKER_LIST = args[0];
-        HTTPServer server = new HTTPServer(1234);
-        log.info("server started on port 1234");
-        new KafkaCollector().register();
+        try (HTTPServer ignored = new HTTPServer(1234)) {
+            new KafkaCollector().register();
+            log.info("server started on port 1234");
+            log.info("Kafka Exporter started");
+            Thread.currentThread().join();
+        } catch (Throwable t) {
+            log.error("Exception starting", t);
+        } finally {
+            log.info("Kafka Exporter exiting");
+        }
     }
 }
