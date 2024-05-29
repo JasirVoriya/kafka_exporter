a5268f7a84d7f607b517f3b3b1bf262bcb0a0217
JasirVoriya <jasirvoriya@gmail.com>
2024-05-29 19:16:52 +0800

    feat:添加注解

--- src/main/java/cn/voriya/kafka/metrics/http/handler/ConfigHandler.java
+++ src/main/java/cn/voriya/kafka/metrics/http/handler/ConfigHandler.java
@@ -10,12 +10,29 @@ import java.io.IOException;
 import java.util.Map;
 
 public class ConfigHandler extends AbstractHttpHandler {
+    /**
+     * 获取配置
+     * query：无
+     * body：无
+     *
+     * @see Config
+     * 返回：Config
+     */
     @Override
     public void get(HttpExchange exchange, Map<String, String> params) throws IOException {
         exchange.sendResponseHeaders(200, 0);
         exchange.getResponseBody().write(JacksonUtil.toJson(Config.getInstance()).getBytes());
     }
 
+    /**
+     * 更新/新增配置
+     * query：无
+     *
+     * @see ConfigCluster
+     * body：ConfigCluster
+     * @see Config
+     * 返回：修改后的Config
+     */
     @Override
     public void post(HttpExchange exchange, Map<String, String> params) throws IOException {
         String body = new String(exchange.getRequestBody().readAllBytes());
@@ -25,6 +42,15 @@ public class ConfigHandler extends AbstractHttpHandler {
         exchange.getResponseBody().write(JacksonUtil.toJson(Config.getInstance()).getBytes());
     }
 
+    /**
+     * 删除配置
+     *
+     * @see String
+     * query：clusterName
+     * body：无
+     * @see Config
+     * 返回：修改后的Config
+     */
     @Override
     public void delete(HttpExchange exchange, Map<String, String> params) throws IOException {
         String clusterName = params.get("clusterName");
