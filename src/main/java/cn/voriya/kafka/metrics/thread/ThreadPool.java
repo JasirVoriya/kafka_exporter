c09e8a0f088473ae9df5b66e79b7716b8c9791a6
JasirVoriya <jasirvoriya@gmail.com>
2024-06-05 18:18:26 +0800

    feat:线程池修改

--- src/main/java/cn/voriya/kafka/metrics/thread/ThreadPool.java
+++ src/main/java/cn/voriya/kafka/metrics/thread/ThreadPool.java
@@ -14,8 +14,8 @@ public class ThreadPool {
             TimeUnit.MINUTES,
             new ArrayBlockingQueue<>(1000),
             new ThreadFactoryBuilder().setNameFormat("kafka-metrics-%d").setDaemon(true).build());
-    public static final ThreadPoolExecutor CONSUMERS_POOL = new ThreadPoolExecutor(5, 5, 0,
+    public static final ThreadPoolExecutor CLUSTER_POOL = new ThreadPoolExecutor(10, 10, 0,
             TimeUnit.MINUTES,
             new ArrayBlockingQueue<>(1000),
-            new ThreadFactoryBuilder().setNameFormat("kafka-consumers-%d").setDaemon(true).build());
+            new ThreadFactoryBuilder().setNameFormat("kafka-clusters-%d").setDaemon(true).build());
 }
