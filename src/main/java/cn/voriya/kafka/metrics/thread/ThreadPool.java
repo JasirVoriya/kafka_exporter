cf489cdd444b3d7cb5fbb283ebf89f50e956396f
JasirVoriya <jasirvoriya@gmail.com>
2024-06-27 11:59:40 +0800

    feat:线程参数调整

--- src/main/java/cn/voriya/kafka/metrics/thread/ThreadPool.java
+++ src/main/java/cn/voriya/kafka/metrics/thread/ThreadPool.java
@@ -5,12 +5,9 @@ import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import java.util.concurrent.*;
 
 public class ThreadPool {
-    public static final ThreadPoolExecutor CLUSTER_POOL = new ThreadPoolExecutor(10, 10, 0,
+    public static final ThreadPoolExecutor CLUSTER_POOL = new ThreadPoolExecutor(5, 5, 0,
             TimeUnit.MINUTES,
             new ArrayBlockingQueue<>(1000),
             new ThreadFactoryBuilder().setNameFormat("kafka-clusters-%d").setDaemon(true).build());
-    public static final ExecutorService VIRTUAL_EXECUTOR = Executors.newFixedThreadPool(
-            Runtime.getRuntime().availableProcessors() * 10000,
-            Thread.ofVirtual().factory()
-    );
+    public static final ExecutorService VIRTUAL_EXECUTOR = Executors.newVirtualThreadPerTaskExecutor();
 }
